55518f8f221b51b5d768a2afb0f7348a672cf732
Heritage Awogbade <haawogbade@upei.ca>
2026-08-27 10:01:59 -0400

    Commit for August 27

---
+++ src/StudentInfo.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class StudentInfo {
+    // Every java project will have only a and single main method
+    // Method = grouping of relevant code and
+    // Return_Type (void) METHOD_NAME (main) (PARAM_Type PARAM_Name){
+
+    // }
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+
+
+
+    }
+}
